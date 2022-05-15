8cfeef9cf36bed580c72860e272bdf0b178f7ccc
vijayaragavan23 <95966836+vijayaragavan23@users.noreply.github.com>
2022-05-15 14:30:23 +0530

    Add files via upload

---
+++ day1/calculator.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
